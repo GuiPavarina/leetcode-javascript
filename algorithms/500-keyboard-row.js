54f452887c9c98aacbc31dd95b28b584601da94d
Guilherme sa <guilhermedesaster@gmail.com>
2019-05-13 21:01:02 -0300

    add 500

---
+++ algorithms/500-keyboard-row.js
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode.com/problems/keyboard-row/
+ * 
+ * @param {string[]} words
+ * @return {string[]}
+ */
+const findWords = (words) => {
+    return words.filter( (el) => {
+        const word = el.toLowerCase();
+        if(checkRows(word, row1)) {
+            return word;
+        }
+    });
+};
+
+const checkRows = (word, row) => isUsingOneRow(word, row1) || isUsingOneRow(word, row2) || isUsingOneRow(word, row3);
+
+const isUsingOneRow = (word, row) => [...word].every( el => row.indexOf(el) !== -1 );
+
+const row1 = [..."qwertyuiop"];
+const row2 = [..."asdfghjkl"];
+const row3 = [..."zxcvbnm"];
+
+findWords(["Hello", "Alaska", "Dad", "Guilherme", "zxcvbnma", "zxcvbnm", "a", "az"]);
